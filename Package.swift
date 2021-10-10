14eb38a8fd6da2fb334f1d35fca63dc5acfdde39
Maxim Kolesnik <kolesnik@yandex-team.ru>
2021-10-10 14:13:27 +0300

    add pack

--- Package.swift
+++ Package.swift
@@ -14,6 +14,8 @@ let package = Package(
             targets: ["monorepo"]),
     ],
     dependencies: [
+        //
+        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.6.0"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -22,10 +24,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "lib",
-            dependencies: []),
+            dependencies: [
+//                "AnyCodable",
+                .product(name: "AnyCodable", package: "AnyCodable")
+            ]),
         .target(
             name: "monorepo",
-            dependencies: []),
+            dependencies: ["lib"]),
         .testTarget(
             name: "monorepoTests",
             dependencies: ["monorepo"]),
